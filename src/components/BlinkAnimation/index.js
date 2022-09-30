d0dafeef246c5f7b54569f81e2cdc2f10592e0c2
AnisaHMohamed <anisahamohamed@gmail.com>
2022-09-30 14:56:03 -0400

    add blink animation

---
+++ src/components/BlinkAnimation/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+const Blink = () => {
+  return (
+    <>
+    <div>Blink Animation</div>
+    <div class="blink">BLINK ME</div>
+
+
+    </>
+  )
+}
+
+
+export default Blink
