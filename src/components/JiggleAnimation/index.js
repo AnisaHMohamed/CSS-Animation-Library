106ac114d183f44d910d0028433987e2bd76c8e3
AnisaHMohamed <anisahamohamed@gmail.com>
2022-10-04 10:44:52 -0400

    add jiggle animation

---
+++ src/components/JiggleAnimation/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+const Jiggle = () => {
+  return (
+    <>
+    <div>Jiggle Animation</div>
+   
+    <div class="jiggle"></div>
+   
+
+    </>
+  )
+}
+
+
+export default Jiggle
